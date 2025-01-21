cdabeb5051b03bb143509d1ea924415f9a170256
Sarthak Shastri <sarthak.shastri@gmail.com>
2025-01-21 14:53:13 -0500

    forgot to push this change

--- bank-demo/src/types.ts
+++ bank-demo/src/types.ts
@@ -5,5 +5,6 @@ export type AccountType = {
 export interface BankType {
     createAccount(username: string, age: number, accountNumber: number): AccountType
     deposit(accountNumber: number, amount: number): Boolean
-
+    withdraw(accountNumber: number, amount: number): Boolean
+    checkBalance(accountNumber: number): number
 }
